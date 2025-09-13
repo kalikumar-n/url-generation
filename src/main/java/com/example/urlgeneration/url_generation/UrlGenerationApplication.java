f093733e2019e40fdcaa0da4398a083328e8a605
kalikumar-n <kalikumar1701@gmail.com>
2025-09-13 12:46:56 +0530

    Initial commit

---
+++ src/main/java/com/example/urlgeneration/url_generation/UrlGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlgeneration.url_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urlgeneration/url_generation/UrlGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlgeneration.url_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
