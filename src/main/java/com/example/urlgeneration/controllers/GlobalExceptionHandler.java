29363713c1ee6a035e99fbd3df1194d7c188f023
kalikumar-n <kalikumar1701@gmail.com>
2025-09-18 07:59:14 +0530

    Added Controller Advice globally to handle the global expection

---
+++ src/main/java/com/example/urlgeneration/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.urlgeneration.controllers;
+
+import com.example.urlgeneration.dtos.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse>handleException(NullPointerException exp){
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                exp.getMessage(),
+                "URLGEN_1001",
+                Instant.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
